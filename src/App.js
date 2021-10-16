bfc8b3151902c1197d7a05c7e82f6d7a53e36964
William Harrison <wm.harrison@gmail.com>
2021-10-16 01:04:04 -0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
-const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const CONTRACT_ADDRESS = "0x34ebD49Ce7474F17846e3Dba86DAbE672c5e1Eda";
 
 const App = () => {
 
