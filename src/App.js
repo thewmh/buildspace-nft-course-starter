98f97ff19cb7ed449c15a12c333890ec5f8bbfc5
William Harrison <wm.harrison@gmail.com>
2021-10-16 00:34:07 -0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () => {
             const account = accounts[0];
             console.log(`Found account: ${account}`);
             setCurrentAccount(account);
-            setupEventListener();
+            // setupEventListener();
         } else {
             console.log("No account found");
         }
@@ -52,58 +52,58 @@ const App = () => {
 
             console.log(`Connected: ${accounts[0]}`);
             setCurrentAccount(accounts[0]);
-            setupEventListener();
+            // setupEventListener();
         } catch (error) {
             console.log(error);
         }
     }
 
-    const setupEventListener = async () => {
-        try {
-            const { ethereum } = window;
+    // const setupEventListener = async () => {
+    //     try {
+    //         const { ethereum } = window;
 
-            if(ethereum) {
-                const provider = new ethers.providers.Web3Provider(ethereum);
-                const signer = provider.getSigner();
-                const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, AINFT.abi, signer);
+    //         if(ethereum) {
+    //             const provider = new ethers.providers.Web3Provider(ethereum);
+    //             const signer = provider.getSigner();
+    //             const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, AINFT.abi, signer);
                 
-                connectedContract.on("NewAINFTMinted", (from, tokenId) => {
-                    console.log(from, tokenId.toNumber());
-                    alert(`NFT minted and sent to your wallet. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`);
-                });
+    //             connectedContract.on("NewAINFTMinted", (from, tokenId) => {
+    //                 console.log(from, tokenId.toNumber());
+    //                 alert(`NFT minted and sent to your wallet. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`);
+    //             });
 
-                console.log("event listener!");
-            } else {
-                console.log("No Etherum object.");
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    // const askContractToMintNft = async () => {
-    //         try {
-    //             const { ethereum } = window;
-
-    //             if (ethereum) {
-    //                 const provider = new ethers.providers.Web3Provider(ethereum);
-    //                 const signer = provider.getSigner();
-    //                 const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, AINFT.abi, signer);
-
-    //                 console.log("pop wallet, pay gas");
-    //                 let nftTransaction = await connectedContract.makeAnAINFT();
-
-    //                 console.log("mining in progress... WAIT!");
-    //                 await nftTransaction.wait();
-
-    //                 console.log(`mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTransaction.hash}`);
-    //             } else {
-    //                 console.log("No Etherum object.");
-    //             }
-    //         } catch (error) {
-    //             console.log(error);
+    //             console.log("event listener!");
+    //         } else {
+    //             console.log("No Etherum object.");
     //         }
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
     // }
+
+    const askContractToMintNft = async () => {
+            try {
+                const { ethereum } = window;
+
+                if (ethereum) {
+                    const provider = new ethers.providers.Web3Provider(ethereum);
+                    const signer = provider.getSigner();
+                    const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, AINFT.abi, signer);
+
+                    console.log("pop wallet, pay gas");
+                    let nftTransaction = await connectedContract.makeAnAINFT();
+
+                    console.log("mining in progress... WAIT!");
+                    await nftTransaction.wait();
+
+                    console.log(`mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTransaction.hash}`);
+                } else {
+                    console.log("No Etherum object.");
+                }
+            } catch (error) {
+                console.log(error);
+            }
+    }
 
     useEffect(() => {
         checkIfWalletIsConnected();
